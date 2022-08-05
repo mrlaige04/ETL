6f243ec8778986933787dd0cf53d113e6db4163e
mrlaige04 <i.rudiakov.ik11@kpi.ua>
2022-08-05 22:30:39 +0300

    FolderReader, CSV Reader, Models

---
+++ Classes/TxtWorker.cs
@@ -0,0 +1,30 @@
+﻿using ETL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETL.Classes
+{
+    internal class TxtWorker
+    {
+        FolderReader folderReader = new FolderReader();
+        public string ReadFile(string path)
+        {
+            using (StreamReader streamReader = new(path))
+            {
+                return streamReader.ReadToEndAsync().Result;
+            }
+        }
+
+        public IEnumerable<T> ConvertToObject<T>(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            Type obj = typeof(T);
+            
+            return null;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ETL;
+using ETL.Classes;
+//FolderReader folderReader = new();
+//IEnumerable<string> files = folderReader.GetFilesPath();
+//FileReader csvreader = new();
+//foreach (var filepath in files)
+//{
+//    Console.WriteLine(csvreader.ReadFile(filepath));
+//    Console.WriteLine(new string('-',50));
+//}
+CSVReader csvreader = new($@"E:\WORK\Radency\ETL\Files\folder_a\payment.csv");
+
+
+IEnumerable<Payer> records = csvreader.GetAllRecords<Payer>();
+
+foreach (var record in records)
+{
+    try
+    {
+        Console.WriteLine(record.ConvertToJson());
+    }
+    catch (MissingFieldException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
