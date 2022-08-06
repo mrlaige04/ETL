3c6155cbc6995cddc7917128308de04513c3b921
mrlaige04 <i.rudiakov.ik11@kpi.ua>
2022-08-06 20:34:26 +0300

    FileManager, TXTConverterDONE

---
+++ Classes/FileManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+namespace ETL.Classes
+{
+    internal class FileManager
+    {
+        FileSystemWatcher CSVwatcher = new(@"..\..\..\Files\folder_a\", "*.csv");
+        FileSystemWatcher TXTwatcher = new(@"..\..\..\Files\folder_a\", "*.txt");
+        string pathOutputBase = @"..\..\..\Files\folder_b\";
+        string pathOutputToday = "";
+        MetaLogWriter metawriter;
+        TXTConverter txtconverter = new();
+        int outputCounter = 0;
+        public FileManager()
+        {
+            pathOutputToday = pathOutputBase + DateTime.Now.ToString("MM/dd/yyyy") + @"\";
+
+
+            CSVwatcher.Created += CSVCreated;
+            TXTwatcher.Created += TXTCreated;
+            CSVwatcher.EnableRaisingEvents = true;
+            TXTwatcher.EnableRaisingEvents = true;
+        }
+
+        private void CSVCreated(object sender, FileSystemEventArgs e)
+        {
+            string value = $"Created CSV: {e.FullPath}";
+            Console.WriteLine(value);
+        }
+
+        private async void TXTCreated(object sender, FileSystemEventArgs e)
+        {
+            Output output = await txtconverter.GetOutputFromTXTFileAsync(e.FullPath);
+            
+            
+            if(!Directory.Exists(pathOutputToday))
+            {
+                Directory.CreateDirectory(pathOutputToday);
+                outputCounter = 0;
+            }
+            
+            using (StreamWriter sw = new(pathOutputToday + "output" + outputCounter + ".json"))
+            {
+                await sw.WriteAsync(JsonSerializer.Serialize(output));
+                outputCounter++;
+            }
+
+            File.Delete(e.FullPath);
+        }       
+
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,3 +2,14 @@
 using ETL.Classes;
 using System.Text.Json;
 
+//FileManager fmanager = new();
+StreamReader sr = new(@"E:\WORK\Radency\ETL\Files\folder_a\data.csv");
+string output = sr.ReadToEnd();
+Console.WriteLine(output);
+CSVConverter csvconverter = new(@"E:\WORK\Radency\ETL\Files\folder_a\data.csv");
+IEnumerable<TransactionDTO> dtos = csvconverter.GetAllRecords<TransactionDTO>();
+foreach (var dto in dtos)
+{
+    Console.WriteLine(dto.ConvertToJSON());
+}
+Console.ReadKey();
